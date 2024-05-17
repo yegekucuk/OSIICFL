d0e7cafbace21c2e52c7cd91f8a816ceebc490b1
yegekucuk <eegekucuk@hotmail.com>
2024-05-17 15:07:55 +0300

    Added Destructor and logger method

--- src/logger/Logger.cpp
+++ src/logger/Logger.cpp
@@ -6,4 +6,28 @@ Logger::Logger(const std::string &filename)
     fileStream.open(filename, std::ofstream::out | std::ofstream::app);
 }
 
-// TODO
+// Destructor
+
+Logger::~Logger()
+{
+    // Check if the file stream is open
+    if (fileStream.is_open())
+    {
+        // Close the file stream
+        fileStream.close();
+    }
+}
+
+// Method to log a message to the file
+void Logger::log(const std::string &message)
+{
+    // Lock the mutex to ensure thread-safe access to the file stream
+    std::lock_guard<std::mutex> guard(mtx);
+    
+    // Check if the file stream is open
+    if (fileStream.is_open())
+    {
+        // Write the message to the file followed by a newline
+        fileStream << message << std::endl;
+    }
+}
